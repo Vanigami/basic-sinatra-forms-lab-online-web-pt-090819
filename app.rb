f51ef1554ebc85298f47f0b602373058f838f31c
Vanna Winland <vgwinland@gmail.com>
2019-12-11 01:37:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,14 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+post '/team' do 
+  @name = params[:name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @sg = params[:sg]
+  @pf = params[:pf]
+  @sf = params[:sf]
+  @c = params[:c]
+end
 
 end
